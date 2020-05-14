d4f44c78c9aacc69cb98d2a31eb7a0a802ab76e4
Dmitriy Solovev <dmptts@gmail.com>
2020-05-15 00:11:35 +0300

    Добавляет простую валидацию форм и стилизацию поля ввода при ошибке

--- source/js/app.js
+++ source/js/app.js
@@ -53,4 +53,76 @@ window.addEventListener("load", function(evt) {
       modalOverlay.classList.remove("modal-overlay--modal-opened");
     };
   });
+
+  // Валидация форм
+
+  let registrationForm = document.querySelector(".feedback__registration");
+  let registrationEmailInput = document.querySelector(".feedback__input");
+
+  registrationEmailInput.removeAttribute("required");
+
+  registrationForm.addEventListener("submit", function(evt) {
+    // registrationEmailInput.classList.remove("feedback__input--error");
+    if (!registrationEmailInput.value) {
+      evt.preventDefault();
+      registrationEmailInput.classList.add("feedback__input--error")
+    };
+  });
+
+  registrationEmailInput.addEventListener("focus", function(evt) {
+    registrationEmailInput.classList.remove("feedback__input--error");
+  });
+
+  // // Убирает атрибут "required" и полей формы фидбека
+
+  // nameInput.removeAttribute("required");
+  // emailInput.removeAttribute("required");
+  // textInput.removeAttribute("required");
+
+  // // Проверка значений формы
+
+  // feedbackForm.addEventListener("submit", function (evt) {
+  //   if (!nameInput.value || !emailInput.value || !textInput.value) {
+  //     evt.preventDefault();
+
+  //     nameInput.classList.remove("modal-feedback-input-err");
+  //     emailInput.classList.remove("modal-feedback-input-err");
+  //     textInput.classList.remove("modal-feedback-input-err");
+  //     modalFeedback.classList.remove("modal-feedback-error");
+  //     modalFeedback.offsetWidth = modalFeedback.offsetWidth;
+  //     modalFeedback.classList.add("modal-feedback-error");
+
+  //     if (!nameInput.value) {
+  //       nameInput.classList.add("modal-feedback-input-err");
+  //     };
+
+  //     if (!emailInput.value) {
+  //       emailInput.classList.add("modal-feedback-input-err");
+  //     };
+
+  //     if (!textInput.value) {
+  //       textInput.classList.add("modal-feedback-input-err");
+  //     };
+
+  //   } else {
+  //     if (isStorageSupport) {
+  //       localStorage.setItem("name", nameInput.value);
+  //       localStorage.setItem("email", emailInput.value);
+  //     };
+  //   };
+  // });
+
+  // // Удаление класса modal-feedback-input-err с элементов формы при фокусе
+
+  // textInput.addEventListener("focus", function (evt) {
+  //   textInput.classList.remove("modal-feedback-input-err");
+  // });
+
+  // emailInput.addEventListener("focus", function (evt) {
+  //   emailInput.classList.remove("modal-feedback-input-err");
+  // });
+
+  // textInput.addEventListener("focus", function (evt) {
+  //   textInput.classList.remove("modal-feedback-input-err");
+  // });
 });
