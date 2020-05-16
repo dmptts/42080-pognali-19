ab98c26114063a03266b4a78e6b0958bd6f5a70f
Dmitriy Solovev <dmptts@gmail.com>
2020-05-16 23:47:50 +0300

    Верстает мобильную версию фильтра по странам

--- source/js/app.js
+++ source/js/app.js
@@ -29,46 +29,69 @@ window.addEventListener("load", function(evt) {
     };
   };
 
+  // Показ/скрытие модального окна
+
   let modal = document.querySelector(".modal-business-rates");
   let ratesLink = document.querySelector(".rates__link");
   let modalOverlay = document.querySelector(".modal-overlay");
   let modalCloseBtn = document.querySelector(".modal-business-rates__btn-close")
 
-  ratesLink.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modal.classList.add("modal-business-rates--opened");
-    modalOverlay.classList.add("modal-overlay--modal-opened");
-  });
+  if (modal !== null || ratesLink !== null) {
+    ratesLink.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      modal.classList.add("modal-business-rates--opened");
+      modalOverlay.classList.add("modal-overlay--modal-opened");
+    });
 
-  modalCloseBtn.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modal.classList.remove("modal-business-rates--opened");
-    modalOverlay.classList.remove("modal-overlay--modal-opened");
-  });
-
-  window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === ESC && modal.classList.contains("modal-business-rates--opened")) {
+    modalCloseBtn.addEventListener("click", function(evt) {
       evt.preventDefault();
       modal.classList.remove("modal-business-rates--opened");
       modalOverlay.classList.remove("modal-overlay--modal-opened");
-    };
-  });
+    });
+
+    window.addEventListener("keydown", function(evt) {
+      if (evt.keyCode === ESC && modal.classList.contains("modal-business-rates--opened")) {
+        evt.preventDefault();
+        modal.classList.remove("modal-business-rates--opened");
+        modalOverlay.classList.remove("modal-overlay--modal-opened");
+      };
+    });
+  }
 
   // Валидация форм
 
   let registrationForm = document.querySelector(".feedback__registration");
   let registrationEmailInput = document.querySelector(".feedback__input");
 
-  registrationEmailInput.removeAttribute("required");
+  if (registrationForm !== null) {
+    registrationEmailInput.removeAttribute("required");
 
-  registrationForm.addEventListener("submit", function(evt) {
-    if (!registrationEmailInput.value) {
+    registrationForm.addEventListener("submit", function(evt) {
+      if (!registrationEmailInput.value) {
+        evt.preventDefault();
+        registrationEmailInput.classList.add("feedback__input--error")
+      };
+    });
+
+    registrationEmailInput.addEventListener("focus", function(evt) {
+      registrationEmailInput.classList.remove("feedback__input--error");
+    });
+  }
+
+
+  // Показ/скрытие фильтра стран
+
+  let filterToggler = document.querySelector(".country-filter__toggle");
+  let filterDropdown = document.querySelector(".country-filter__dropdown");
+
+  if (filterToggler !== null || filterDropdown !== null) {
+    filterDropdown.classList.remove("country-filter__dropdown--no-js");
+    filterToggler.classList.remove("country-filter__toggle--no-js");
+
+    filterToggler.addEventListener("click", function(evt) {
       evt.preventDefault();
-      registrationEmailInput.classList.add("feedback__input--error")
-    };
-  });
-
-  registrationEmailInput.addEventListener("focus", function(evt) {
-    registrationEmailInput.classList.remove("feedback__input--error");
-  });
+      filterDropdown.classList.toggle("country-filter__dropdown--opened");
+      filterToggler.classList.toggle("country-filter__toggle--opened");
+    })
+  }
 });
